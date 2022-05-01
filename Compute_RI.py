543968bb74762e3f9fa08ba3c5d6c3e48f8365b6
Arnault Pachot <apachot@openstudio.fr>
2022-05-01 07:44:33 +0200

    Compute RI for 39338 french officies

--- Compute_RI.py
+++ Compute_RI.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import geopy.distance
-
+import codecs
 
 def suppliersFromNaF(naf):
 	IO_file="./input/IO-matrix.csv"
@@ -17,7 +17,6 @@ def suppliersFromNaF(naf):
 
 def scoreLocalSuppliersFromNaf(naf, coord_x, coord_y, threshold):
 	
-	#extract GPS coordinates from siret
 	coords_naf = (coord_x,coord_y)
 
 	#extract NAF code for suppliers according to IO tables 
@@ -27,7 +26,7 @@ def scoreLocalSuppliersFromNaf(naf, coord_x, coord_y, threshold):
 
 	for i in range(0, len(naf_suppliers)):
 		#extract list of suppliers with each naf code
-		officies_file="./input/etablishments-france-department43.csv"
+		officies_file="./input/offices-france.csv"
 		officies_table = pd.read_csv(officies_file, sep="," ,header=1, index_col=None).to_numpy()
 		code_naf_supplier = naf_suppliers[i][0]
 		#print(officies_table)
@@ -49,7 +48,7 @@ def scoreLocalSuppliersFromNaf(naf, coord_x, coord_y, threshold):
 			score = score + float(naf_suppliers[i][1])
 	
 
-	return score * 5
+	return min(score,1)
 
 def scoreProductsFromLocalSuppliersFromNaf(naf):
 	
@@ -91,18 +90,18 @@ def scoreProductsFromLocalSuppliersFromNaf(naf):
 		score = score + weight_naf_supplier * total_score_supplier
 
 	#print("total_ score pour cet établissement =", score)
-	return score
+	return min(score,1)
 
 def scoreDiversificationFromNaf(naf, threshold):
 
-	score = 0
-
+	
 	#first we look for products associated with this naf code
 	NAF_HS4_file = "./input/NAF_HS4.csv"
 	products_table = pd.read_csv(NAF_HS4_file, sep="," ,header=1, index_col=None).to_numpy()
 		
 	products_table_naf = [[a[1],a[2]] for a in products_table if int(a[0][0:4]) == int(str(naf).split('.')[0]+str(naf).split('.')[1])]
 		
+	#print("products_table_naf", products_table_naf)
 	score = 0
 
 	for j in range(0, len(products_table_naf)):
@@ -116,10 +115,11 @@ def scoreDiversificationFromNaf(naf, threshold):
 		#print("tab_proximities_prod", tab_proximities_prod)
 		if (len(tab_proximities_prod) > 0):
 			score = score + prod_weight
+		#print(score)
 
 	#print("score diversification=", score)
 
-	return score
+	return min(score,1)
 
 def scoreDiversificationHRFromNaf(naf, threshold):
 	score = 0
@@ -136,6 +136,7 @@ def scoreDiversificationHRFromNaf(naf, threshold):
 	for i in range(0, len(NAF_ROME_tab_naf)):
 		code_rome = NAF_ROME_tab_naf[i][0]
 		code_rome_weight = NAF_ROME_tab_naf[i][1]
+		#print("code_rome_weight", code_rome_weight)
 
 		#print("métier trouvé=", code_rome, "weight=", code_rome_weight)
 
@@ -148,10 +149,12 @@ def scoreDiversificationHRFromNaf(naf, threshold):
 		if (len(ROME_proximities_tab_rome)):
 			score = score + code_rome_weight * (len(ROME_proximities_tab_rome) / max_jumps)
 
+			#print("score=",score, "code_rome_weight", code_rome_weight, "len(ROME_proximities_tab_rome)", len(ROME_proximities_tab_rome))
+
 		
 
 	#print("score diversification rh=", score)
-	return score
+	return min(score,1)
 
 def compute_RI(F_1_4, F_2_2, F_2_4, F_3_3):
 	return F_1_4 * (F_2_2 * F_2_4 + 20) * F_3_3 + 42 * F_1_4 + 72 * (F_2_2 * F_2_4 + 20) + 40 * F_3_3 + 718 
@@ -182,16 +185,16 @@ def compute_F_3_3(naf):
 	return F_3_3
 
 
-def compute_siren_RI(naf, coord_x, coord_y):
+def compute_naf_RI(naf, coord_x, coord_y):
 	
 
 	#look for naf code
-	F_1_4 = compute_F_1_4(naf, coord_x, coord_y, 100)
-	F_2_2 = compute_F_2_2(naf, 0.5)
+	F_1_4 = compute_F_1_4(naf, coord_x, coord_y, 200)
+	F_2_2 = compute_F_2_2(naf, 0.8)
 	F_2_4 = compute_F_2_4(naf, 0.9)
 	#F_3_1 = compute_F_3_1(naf, )
 	F_3_3 = compute_F_3_3(naf)
-	return compute_RI(F_1_4, F_2_2, F_2_4, F_3_3)
+	return compute_RI(F_1_4, F_2_2, F_2_4, F_3_3), F_1_4, F_2_2, F_2_4, F_3_3
 
 
 
@@ -201,10 +204,30 @@ naf = 13.96
 coord_x = 45.244352
 coord_y = 4.271605
 
-suppliers = suppliersFromNaF(naf)
-#print("suppliers=", suppliers)
+all_officies_file="./input/offices-france.csv"
+all_officies_table = pd.read_csv(all_officies_file, sep="," ,header=1, index_col=None).to_numpy()
+
+NAF_HS4_file = "./input/NAF_HS4.csv"
+products_table = pd.read_csv(NAF_HS4_file, sep="," ,header=1, index_col=None).to_numpy()
+
+output_filename = "./output/computed_RI_for_officies.csv"
+with codecs.open(output_filename, "w", "utf8") as o:
+	o.write(f"siret,naf,ri,F_1_4,F_2_2,F_2_4,F_3_3\n")
+	for i in range(0, len(all_officies_table)):
+		siret = all_officies_table[i][0]
+		naf = float(all_officies_table[i][1][0:5])
+		coord_x = float(str(all_officies_table[i][3].split(',')[0][1:]))
+		coord_y = float(str(all_officies_table[i][3].split(',')[1][0:-1]))
+		
+		#we verify that thos etabilishment is industrial
+		products_table_naf = [[a[1],a[2]] for a in products_table if int(a[0][0:4]) == int(str(naf).split('.')[0]+str(naf).split('.')[1])]
+	
+		if(len(products_table_naf)):
+			#print("(", i ,"/", len(all_officies_table),": Etablissement", all_officies_table[i][4], " (", naf, ")")
+			#print("coord_x=", coord_x, "coord_y=", coord_y, "origin=", all_officies_table[i][3])
+			RI, F_1_4, F_2_2, F_2_4, F_3_3 = compute_naf_RI(naf, coord_x, coord_y)
+			print("[" , i , "/" , len(all_officies_table) , "]" , round(RI,2),round(F_1_4,2),round(F_2_2,2),round(F_2_4,2),round(F_3_3,2),"(siret=",siret,")")
+			o.write(f"{siret},{all_officies_table[i][1]},{round(RI,2)},{round(F_1_4,2)},{round(F_2_2,2)},{round(F_2_4,2)},{round(F_3_3,2)}\n")
+			o.flush()
 
 
-
-RI = compute_siren_RI(naf, coord_x, coord_y)
-print("RI=", RI)
